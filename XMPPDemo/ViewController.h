c754153ce4e957bff47451a2b26fe6f3de4c4fbd
xieqilin <460565420@qq.com>
2018-06-05 09:51:18 +0800

    Initial Commit

---
+++ XMPPDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XMPPDemo
+//
+//  Created by xieqilin on 2018/6/5.
+//  Copyright © 2018年 xieqilin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
